2ba0f548cc3c8dea8e9935110abe084b5ba0be3a
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-13 22:00:56 -0300

    Rotas de criação e listagem (Métodos HTTP)

--- src/server.js
+++ src/server.js
@@ -4,6 +4,15 @@ import http from "node:http";
 //Response: Oq eu envio com oq recebo.
 
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+  if (method === "GET" && url === "/users") {
+    return response.end("listagem de usuarios");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("criação de usuario");
+  }
+
   return response.end("Hello word");
 });
 
