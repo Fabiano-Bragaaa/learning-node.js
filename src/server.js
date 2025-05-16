77fea58a9d8529eeeb06f28c9de884c1c1f8240a
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-15 21:08:40 -0300

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -3,13 +3,22 @@ import http from "node:http";
 //Req: Oq eu recebo.  criar um usuario (name, email, password)
 //Response: Oq eu envio com oq recebo.
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
   if (method === "GET" && url === "/users") {
-    return response.end("listagem de usuarios");
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+    });
     return response.end("criação de usuario");
   }
 
