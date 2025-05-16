0e7c4cacb696faf4ba660d5a92e26666404304f5
Fabiano Braga <fabianobragadev@gmail.com>
2025-05-16 20:53:09 -0300

    Criando stream de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,1 +1,21 @@
-process.stdin.pipe(process.stdout);
+import { Readable } from "node:stream";
+
+class oneToHundreadStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buf = Buffer.from(String(i));
+
+        this.push(buf);
+      }
+    }, 1000);
+  }
+}
+
+new oneToHundreadStream().pipe(process.stdout);
